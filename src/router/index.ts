144ef7f4dfcee13ca35cb512707e799cd1d260cb
Nelly Aviles <143915388+Sodi0@users.noreply.github.com>
2025-09-30 02:39:32 -0300

    feat: agregar rutas para las vistas Home, Mapa, Estadisticas y Contacto

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-
-]
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("../views/Home.vue"),
+  },
+  {
+    path: "/mapa",
+    name: "Mapa",
+    component: () => import("../views/Mapa.vue"),
+  },
+  {
+    path: "/estadisticas",
+    name: "Estadisticas",
+    component: () => import("../views/Estadisticas.vue"),
+  },
+  {
+    path: "/contacto",
+    name: "Contacto",
+    component: () => import("../views/Contacto.vue"),
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
