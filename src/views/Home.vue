873293792cfacf660d94c28c241fba3fdb4f6efb
Nelly Aviles <143915388+Sodi0@users.noreply.github.com>
2025-09-30 02:39:23 -0300

    feat: agregar vista Home con componentes Hero y BuscadorEstaciones

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <Hero />
+    <BuscadorEstaciones />
+</template>
+
+<script setup lang="ts">
+import Hero from '../components/Home/Hero.vue';
+import BuscadorEstaciones from '../components/Home/BuscadorEstaciones.vue';
+</script>
