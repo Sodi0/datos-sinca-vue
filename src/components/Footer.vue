de53ec250490c3a8864282b6f4a853327b100aa5
Nelly Aviles <143915388+Sodi0@users.noreply.github.com>
2025-09-30 02:38:31 -0300

    fix: actualizar el nombre en el pie de página a 'Visualizador de datos SINCA'

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="bg-gradient-to-r from-gray-900 via-blue-900 to-gray-900 text-white mt-auto p-6 text-center w-full">
-    <p>© {{ new Date().getFullYear() }} Chile Respira</p>
+    <p>© {{ new Date().getFullYear() }} Visualizador de datos SINCA</p>
     <p>
       Datos públicos del
       <a href="https://sinca.mma.gob.cl/" target="_blank" class="underline text-blue-300">SINCA - MMA Chile</a>
